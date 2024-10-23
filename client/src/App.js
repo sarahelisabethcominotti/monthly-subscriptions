32fe7a556eab784f7bd89a8af22b1b14a8909939
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-10-23 16:28:56 +0100

    addition of add and stats button to the header

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import './App.css';
+import "./App.css";
 import { Calendar } from "./components/Calendar.js";
 // import Subscriptions from "./components/Subscriptions.js";
 
-
 function App() {
-
-
   return (
     <div className="App">
-      
       <header className="App-header">
+        <button className="header-icon">
+          <span className="material-symbols-rounded">add</span>
+        </button>
         <p>My Monthly Subscriptions</p>
+        <button
+          className="header-icon"
+        >
+          <span className="material-symbols-rounded">bar_chart</span>
+        </button>
       </header>
       <main className="App-main">
-        <Calendar/>
+        <Calendar />
         {/* <Subscriptions/> */}
       </main>
     </div>
