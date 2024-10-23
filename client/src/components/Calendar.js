21618c55572b97857509b097ec9a81a2028d5386
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-10-23 15:36:42 +0100

    fixing of deployment bugs

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -16,7 +16,7 @@ export const Calendar = () => {
     Axios.get(`${apiUrl}/getSubscriptions`).then((response) => {
       setListOfSubscriptions(response.data);
     });
-  }, []);
+  }, [apiUrl]);
 
   const createSubscription = () => {
     Axios.post(`${apiUrl}/createSubscription`, {
